51f871d172734943f5deb694b463a09836820bfd
RahulDeoRanjan <onlyrdr@gmail.com>
2025-01-26 08:49:28 +0530

    usings order correction

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,8 @@
-from model.model import train_model
 import sys
 import os
+from model.model import train_model
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../model')))
 
 def test_model_accuracy():
     accuracy = train_model()
     assert accuracy > 0.2
-    
