7c14865dd5af4655f5280b96c5744bfd8325342f
RahulDeoRanjan <onlyrdr@gmail.com>
2025-01-26 08:45:22 +0530

    corrections done

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,9 @@
+from model.model import train_model
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../model')))
 
-from ..model import train_model
-
 def test_model_accuracy():
     accuracy = train_model()
     assert accuracy > 0.2
+    
